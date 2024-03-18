ef701fd639a896f6add9c6fa9b1965d9cd9ab7d7
NewPaulWalker <linzhida19@mails.ucas.ac.cn>
2024-03-18 11:17:26 +0800

    chap2: finish ex2_2

--- code_chap_2_3/code_chap_2_3_student/exp_2_2_mnist_mlp_dlp/stu_upload/mnist_mlp_demo.py
+++ code_chap_2_3/code_chap_2_3_student/exp_2_2_mnist_mlp_dlp/stu_upload/mnist_mlp_demo.py
@@ -43,8 +43,59 @@ class MNIST_MLP(object):
         output_shapem1[3]=hidden1
     
         self.net.createMlpLayer('fc1', input_shapem1, weight_shapem1, output_shapem1)
-        __________________ 
+        # relu1
+        self.net.createReLuLayer('relu1')
 
+        # fc2 
+        input_shapem2=pycnnl.IntVector(4)  
+        input_shapem2[0]=batch_size
+        input_shapem2[1]=1
+        input_shapem2[2]=1
+        input_shapem2[3]=hidden1
+        weight_shapem2=pycnnl.IntVector(4)  
+        weight_shapem2[0]=batch_size
+        weight_shapem2[1]=1
+        weight_shapem2[2]=hidden1
+        weight_shapem2[3]=hidden2
+
+        output_shapem2=pycnnl.IntVector(4)  
+        output_shapem2[0]=batch_size
+        output_shapem2[1]=1
+        output_shapem2[2]=1
+        output_shapem2[3]=hidden2
+    
+        self.net.createMlpLayer('fc2', input_shapem2, weight_shapem2, output_shapem2)
+
+        # relu2
+        self.net.createReLuLayer('relu2')
+
+        # fc3 
+        input_shapem3=pycnnl.IntVector(4)  
+        input_shapem3[0]=batch_size
+        input_shapem3[1]=1
+        input_shapem3[2]=1
+        input_shapem3[3]=hidden2
+        weight_shapem3=pycnnl.IntVector(4)  
+        weight_shapem3[0]=batch_size
+        weight_shapem3[1]=1
+        weight_shapem3[2]=hidden2
+        weight_shapem3[3]=out_classes
+
+        output_shapem3=pycnnl.IntVector(4)  
+        output_shapem3[0]=batch_size
+        output_shapem3[1]=1
+        output_shapem3[2]=1
+        output_shapem3[3]=out_classes
+    
+        self.net.createMlpLayer('fc3', input_shapem3, weight_shapem3, output_shapem3)
+
+        #softmax
+        input_shapes1=pycnnl.IntVector(3)
+        input_shapes1[0]=batch_size
+        input_shapes1[1]=1
+        input_shapes1[2]=out_classes
+        axis_s1=1
+        self.net.createSoftmaxLayer('softmax', input_shapes1, axis_s1)
     
     def load_mnist(self, file_dir, is_images = 'True'):
         # Read binary data
@@ -69,8 +120,8 @@ class MNIST_MLP(object):
     
     def load_data(self, data_path, label_path):
         print('Loading MNIST data from files...')
-        test_images = ________________________
-        test_labels = ________________________
+        test_images = self.load_mnist(data_path, True)
+        test_labels = self.load_mnist(label_path, False)
         self.test_data = np.append(test_images, test_labels, axis=1)
 
     def load_model(self, param_dir):   # 加载参数
@@ -84,11 +135,11 @@ class MNIST_MLP(object):
         
         weigh2 = params['w2'].flatten().astype(np.float64)
         bias2 = params['b2'].flatten().astype(np.float64)
-        ____________________
+        self.net.loadParams(2,weigh2, bias2)
 
         weigh3 = params['w3'].flatten().astype(np.float64)
         bias3 = params['b3'].flatten().astype(np.float64)
-        ____________________
+        self.net.loadParams(4, weigh3, bias3)
 
            
     def forward(self):
